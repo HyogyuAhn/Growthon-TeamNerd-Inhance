7ae163944aa2cf7ea32c2789d29ec51628a3703e
안효규 <hyogyu@inha.edu>
2024-05-30 02:32:42 +0900

    Update IntroActivty
    기존의 그라데이션 배경의 일반 로고만 보이는 방식에서 흰색 배경위에 로고 글씨가 작성되는 형식으로 변경

--- app/src/main/java/nerd/example/inha_project/IntroActivity.java
+++ app/src/main/java/nerd/example/inha_project/IntroActivity.java
@@ -1,10 +1,20 @@
 package nerd.example.inha_project;
 
+import android.animation.ArgbEvaluator;
+import android.animation.ValueAnimator;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.RelativeSizeSpan;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -13,6 +23,11 @@ import androidx.core.view.WindowInsetsCompat;
 import nerd.example.inha_project.account.LoginActivity;
 
 public class IntroActivity extends AppCompatActivity {
+
+    private static final String INTRO_TEXT = "INHA+nce";
+    private TextView textIntro;
+    private int index = 0;
+    private Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,17 +40,69 @@ public class IntroActivity extends AppCompatActivity {
             return insets;
         });
 
-        Handler handler = new Handler();
-        handler.postDelayed(() -> {
-            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
-            startActivity(intent);
-            finish();
-        }, 3000);
+        textIntro = (TextView) findViewById(R.id.intro_logo);
+
+        handler.postDelayed(textAdder, 200);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
         finish();
+    }
+
+    private final Runnable textAdder = new Runnable() {
+        @Override
+        public void run() {
+            if (index <= INTRO_TEXT.length()) {
+                textIntro.setText(INTRO_TEXT.substring(0, index));
+                index++;
+                handler.postDelayed(this, 120);
+            } else {
+                handler.postDelayed(changeText, 850);
+            }
+        }
+    };
+
+    private Runnable changeText = new Runnable() {
+        @Override
+        public void run() {
+            SpannableString spannableString = new SpannableString(INTRO_TEXT);
+
+            String changeText = "A+";
+            int start = INTRO_TEXT.indexOf(changeText);
+            int end = start + changeText.length();
+
+            spannableString.setSpan(new ForegroundColorSpan(Color.parseColor("#4285F4")), start, end, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+            spannableString.setSpan(new RelativeSizeSpan(1.2f), start, end, SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+            textIntro.setText(spannableString);
+
+            textIntro.post(new Runnable() {
+                @Override
+                public void run() {
+                    RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) textIntro.getLayoutParams();
+                    params.topMargin -= dpToPx(21);
+                    textIntro.setLayoutParams(params);
+                }
+            });
+
+
+            handler.postDelayed(startNextActivity, 2500);
+        }
+    };
+
+    private final Runnable startNextActivity = new Runnable() {
+        @Override
+        public void run() {
+            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    };
+
+    private int dpToPx(int dp) {
+        float density = getResources().getDisplayMetrics().density;
+        return Math.round(dp * density);
     }
 }
